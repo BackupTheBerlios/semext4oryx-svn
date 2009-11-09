3a13c0985897c6f21967ab73d026d67e222feb4d
jboettcher <jboettcher@49d1dab9-d076-0410-8c01-bd84892f8566>
2009-11-09 08:05:27 +0000

    initial commit

---
+++ trunk/oryx/editor/test/client/Core/fixtures/StencilSet/testStencilSetExtensionWithProps.js
@@ -0,0 +1,32 @@
+var testStencilSetExtensionPropJSON = {
+	"title":"TestStencilSetExtention",
+	"namespace":"http://testStencilSetExtension#",
+	"description":"Test extension",
+	"extends":"http://b3mn.org/stencilset/testB#",
+	"stencils":[],
+	"properties":[
+		{
+			"roles": [
+				"Service1",
+				"Service2"
+			],
+			"properties": [
+				{
+					"id":"testExtensionProp",
+					"type":"Integer",
+					"title":"extensionTestProp",
+					"description":"test test"
+				}
+			]
+		}
+	],
+	"rules": {
+		"connectionRules": [],
+        "cardinalityRules": [],
+		"containmentRules": []
+	},
+	"removestencils": [
+		
+	],
+	"removeproperties": []
+}
